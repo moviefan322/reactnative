5baf188c0f0f8d4f576c17d37c2f8ac311e9e308
moviefan322 <philman202@yahoo.com>
2024-11-10 16:04:46 -0500

    modifies layout w flexbox attrs

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -7,8 +7,8 @@ export default function App() {
         <TextInput style={styles.textInput} placeholder="Your Goal" />
         <Button title="Add Goal" />
       </View>
-      <View>
-        <Text>List of Goals!...</Text>
+      <View style={styles.goalsContainer}>
+        <Text>List of Goals...</Text>
       </View>
     </View>
   );
@@ -16,17 +16,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
+    flex: 1,
     padding: 50,
+    paddingHorizontal: 40,
   },
   inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
   textInput: {
-    borderColor: '#cccccc',
+    borderColor: "#cccccc",
     borderWidth: 1,
     padding: 8,
-    width: '80%',
+    width: "80%",
     marginRight: 8,
-  }
+  },
+  goalsContainer: {
+    flex: 5,
+  },
 });
