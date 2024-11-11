d56c93965dcc43d87c870729c9730429c1fc2d3f
moviefan322 <philman202@yahoo.com>
2024-11-10 23:13:26 -0500

    outlines basic screens

---
+++ starting-project/screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { View, TextInput, Text, StyleSheet } from "react-native";
+
+const StartGameScreen = () => {
+  return (
+    <View>
+      <TextInput />
+      BUTTONS
+    </View>
+  );
+};
+
+export default StartGameScreen;
