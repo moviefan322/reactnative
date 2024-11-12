1dc08bceb2eed256aa3751ae03f7b880fb014ca3
moviefan322 <philman202@yahoo.com>
2024-11-12 01:26:46 -0500

    uses SafeAreaView

--- starting-project/screens/GameScreen.js
+++ starting-project/screens/GameScreen.js
@@ -1,7 +1,23 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 const GameScreen = () => {
-  return <Text>Game Screen!</Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>Opponents guess!</Text>
+      {/* GUESS */}
+      <View>
+        <Text>Higher or Lower</Text>
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
+    </View>
+  );
 };
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
