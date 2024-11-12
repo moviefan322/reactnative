0f95b544c09142812baac4ac57e1535199cfbc99
moviefan322 <philman202@yahoo.com>
2024-11-12 02:00:50 -0500

    adds logic to guess random number w params

--- starting-project/screens/GameScreen.js
+++ starting-project/screens/GameScreen.js
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
-import Title from "../components/Title";
 
-const GameScreen = () => {
+import Title from "../components/ui/Title";
+import NumberContainer from "../components/game/NumberContainer";
+
+const generateRandomNumber = (min, max, exclude) => {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomNumber(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+};
+
+const GameScreen = ({ userNumber }) => {
+  const initialGuess = generateRandomNumber(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
   return (
     <View style={styles.screen}>
       <Title>Opponents guess!</Title>
-      {/* GUESS */}
+      <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or Lower</Text>
       </View>
